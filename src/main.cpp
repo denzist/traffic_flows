c50df086ba65c64cb8d6668816be77cb5e6f9595
denzist <denizst@gmail.com>
2015-11-24 00:31:26 +0300

    minor fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,48 +7,47 @@ int main() {
   std::vector<PVertex> v_ptr_vec(4);
   for(int i = 0; i < 4; ++i)
   {
+    v_ptr_vec[i] = Vertex::createPVertex();
     graph.insert(std::pair<PVertex, EdgeVec>(v_ptr_vec[i], EdgeVec()));  
   }
   
+  EdgeInfo edge_info_0_1(2., 3., 1.);
+  graph[v_ptr_vec[0]].push_back(Edge(v_ptr_vec[1], EdgeInfo(1., 3., 1.)));
+  graph[v_ptr_vec[0]].push_back(Edge(v_ptr_vec[2], EdgeInfo(4., 3., 1.)));
 
-  // EdgeInfo edge_info_0_1(2., 3., 1.);
-  // graph[0]->add_edge(Edge(graph[1], EdgeInfo(1., 3., 1.)));
-  // graph[0]->add_edge(Edge(graph[2], EdgeInfo(4., 3., 1.)));
+  graph[v_ptr_vec[1]].push_back(Edge(v_ptr_vec[2], EdgeInfo(1., 3., 1.)));
+  graph[v_ptr_vec[1]].push_back(Edge(v_ptr_vec[3], EdgeInfo(4., 3., 1.)));
 
-  // graph[1]->add_edge(Edge(graph[2], EdgeInfo(1., 3., 1.)));
-  // graph[1]->add_edge(Edge(graph[3], EdgeInfo(4., 3., 1.)));
+  graph[v_ptr_vec[2]].push_back(Edge(v_ptr_vec[3], EdgeInfo(1., 3., 1.)));
 
-  // graph[2]->add_edge(Edge(graph[3], EdgeInfo(1., 3., 1.)));
+  for (auto it = graph.begin(); it != graph.end(); ++it)
+  {
+    std::cout<<"i -- "<<it->first->get_id()<<" : ";   
+    for(auto it_e = it->second.begin(); it_e != it->second.end(); ++it_e)
+    {
+      std::cout<<it_e->first->get_id()<<" -- ";   
+      std::cout<<it_e->second.get_cost()<<"; ";   
+    }
+    std::cout<<"\n";
+  }
 
+  auto path_ptr = Dijkstra::get_shortest_path(graph, v_ptr_vec[0], v_ptr_vec[3]);
+  double sum = 0;
+  std::cout<<"size: "<<path_ptr->size()<<"\n";
+  auto end = path_ptr->end();
+  --end;
+  std::cout<<"i: "<<(*path_ptr->begin())->get_id()<<"\n";
+  for (auto it = path_ptr->begin(); it != end; ++it)
+  {
+    auto next = it;
+    ++next;
+    EdgeInfo& info = graph[*next].second;
+    std::cout<<"cost: "<<info.get_cost()<<"\n";
+    std::cout<<"i: "<<*next<<"\n";
+    sum += info.get_cost();
+  }
 
-  // for(int i = 0; i < graph.size(); ++i)
-  // {
-  //   std::cout<<"i -- "<<graph[i]<<" : ";   
-  //   for(auto it = graph[i]->get_edges().begin(); it != graph[i]->get_edges().end(); ++it)
-  //   {
-  //     std::cout<<it->second.get_cost()<<"; ";   
-  //   }
-  //   std::cout<<"\n";
-
-  // }
-
-  // auto path_ptr = Dijkstra::get_shortest_path(graph, graph[0], graph[3]);
-  // double sum = 0;
-  // std::cout<<"size: "<<path_ptr->size()<<"\n";
-  // auto end = path_ptr->end();
-  // --end;
-  // std::cout<<"i: "<<*(path_ptr->begin())<<"\n";
-  // for (auto it = path_ptr->begin(); it != end; ++it)
-  // {
-  //   auto next = it;
-  //   ++next;
-  //   EdgeInfo& info = (*it)->get_edges()[*next];
-  //   std::cout<<"cost: "<<info.get_cost()<<"\n";
-  //   std::cout<<"i: "<<*next<<"\n";
-  //   sum += info.get_cost();
-  // }
-
-  // std::cout<<"path cost: "<<sum<<"\n";
+  std::cout<<"path cost: "<<sum<<"\n";
 
 
   return 0;
